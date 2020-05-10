dff7c70b40d3e9b2c4249721cbdbeea8d00802a3
Nicolas Estrada <59927633+estradanicolas@users.noreply.github.com>
2020-05-09 21:52:01 -0500

    Add files via upload

---
+++ build/precache-manifest.0a4895339356b8866375da69eae5f5a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aefded812f92e318df0039061b1b924",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65723eb2149c525d03fb",
+    "url": "/static/css/main.727e64e3.chunk.css"
+  },
+  {
+    "revision": "e89f7dabdbfbffccea5b",
+    "url": "/static/js/2.50d0b98e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.50d0b98e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65723eb2149c525d03fb",
+    "url": "/static/js/main.77607ebd.chunk.js"
+  },
+  {
+    "revision": "29e3e01435207bdb4a73",
+    "url": "/static/js/runtime-main.4e0ead85.js"
+  },
+  {
+    "revision": "ba6c84c367f52fa228922691ad4d31ed",
+    "url": "/static/media/background_search_desktop.ba6c84c3.jpg"
+  },
+  {
+    "revision": "356bb80c8bfbc8975f1232ba14309e64",
+    "url": "/static/media/background_search_mobile.356bb80c.jpg"
+  }
+]);
---
+++ util/Yelp.js
@@ -0,0 +1,29 @@
+const apiKey = 'EP5EVNSVNtn7B26aR37CHvGGjZc2K1qgmRI4q1pJMpZkxqez6Dgea5n-tn2u7SWBoPu5K-CEuEXf562vhBK2kQF5nRFKq7QjyPNgfFVvm1g5vpmDlzX5EBYngcWpXnYx';
+const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${apiKey}`}
+    }).then(response => {
+        return response.json();
+    }).then(jsonResponse => {
+        if (jsonResponse.businesses) {
+            return jsonResponse.businesses.map(business => {
+                console.log(business);
+                return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zipcode,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.reviewCount,
+                }
+            });
+
+        }
+    });
+    }
+};
+export default Yelp;
